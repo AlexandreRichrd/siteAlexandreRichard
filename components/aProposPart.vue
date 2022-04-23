873c37f93114703562a6caead357adfc19a03dee
Alexandre <alexandre.richard49410@gmail.com>
2022-04-23 20:46:55 +0200

    first commit

--- components/aProposPart.vue
+++ components/aProposPart.vue
@@ -1,7 +1,24 @@
 <template>
   <div id="about">
     <h1>À propos</h1>
-    <h2></h2>
+    <h2>Alexandre RICHARD, 20 ans et toutes ses dents !</h2>
+    <div class="logoEtTexte">
+      <div class="texte">
+        <h3>Un développeur web passionné !</h3>
+        <p>Actuellement en BTS Systèmes Numériques, j’ai découvert  le développement web il y a 5 ans au CDI de mon lycée.</p>
+        <p>Je me rappelle encore rentrer des cours et me précipiter sur l’ordinateur pour suivre des cours sur openclassroom.</p>
+        <p>J’y ai appris les bases du HTML, du CSS, du PHP et du MySQL.</p>
+        <p>J’ai au cours de ma deuxième année d’apprentissage,  j’ai commencé à investir dans des cours sur la plateforme Udemy avec des cours plus longs et plus précis. J’y ai d’ailleurs découvert Javascript.</p>
+        <p>Puis au cours de la dernière année, j’ai commencé à suivre des cours sur Dyma où j’ai découvert le framework Javascript VueJS.</p>
+      </div>
+      <img src="@/static/images/logo.svg" alt="logo">
+      <div class="texte">
+        <h3>Mon expérience en développement</h3>
+        <p>J’ai pendant plus de 4 ans développé des site web “pour le fun”.</p>
+        <p>En effet, ma première mission de développeur web s’est déroulée durant mon stage de première année de BTS. J’ai en effet conçu une application web en HTML, CSS, PHP et MySQLi pour le collège et lycée Saint Gab’.</p>
+        <p>Ce milieu m’ayant bien plu, j’ai décidé, en février 2022, de créer mon entreprise et c’est ce qui marqua le début de mon aventure d’auto-entrepreneur.</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,13 +33,37 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 5rem 0 0 0;
+  padding: 7rem 0 10rem 0;
 }
 
 h1{
   text-transform: uppercase;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   font-weight: 500;
   font-size: 2.5em;
+}
+
+h2{
+  color: var(--sub-title-color);
+  font-weight: 400;
+  font-size: 2em;
+}
+
+h3{
+  font-size: 1.5em;
+  font-weight: 400;
+  color: var(--sub-title-color);
+}
+
+p{
+  color: var(--sub-title-color);
+  width: 22rem;
+  text-align: justify;
+}
+
+.logoEtTexte{
+  margin-top: 10rem;
+  display: flex;
+  justify-content: center;
+  gap: 10rem;
 }
 </style>
--- layouts/defaultPage.vue
+++ layouts/defaultPage.vue
@@ -8,9 +8,11 @@ body {
   margin: 0;
   padding: 0;
   height: 100vh;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
 :root{
   --background-color: #DCDCDC;
+  --sub-title-color: #7F7F7F;
 }
 </style>
